f0bae728d937a581e721b844c7351f6ca8eef4e3
kovinevmv <kovinevmv@gmail.com>
2020-03-12 22:33:58 +0300

    feat: add const to table names and columns

--- src/app/src/main/java/com/leti/phonedetector/database/DBContract.kt
+++ src/app/src/main/java/com/leti/phonedetector/database/DBContract.kt
@@ -7,42 +7,42 @@ object DBContract {
     /* Inner class that defines the table contents */
     class PhoneInfoEntry : BaseColumns {
         companion object {
-            val TABLE_NAME = "info"
-            val COLUMN_INFO_PHONE_NAME = "name"
-            val COLUMN_INFO_PHONE_NUMBER = "number"
-            val COLUMN_INFO_PHONE_IS_SPAM = "isSpam"
-            val COLUMN_INFO_PHONE_IMAGE = "image"
+            const val TABLE_NAME = "info"
+            const val COLUMN_INFO_PHONE_NAME = "name"
+            const val COLUMN_INFO_PHONE_NUMBER = "number"
+            const val COLUMN_INFO_PHONE_IS_SPAM = "isSpam"
+            const val COLUMN_INFO_PHONE_IMAGE = "image"
         }
     }
 
     class PhoneLogEntry : BaseColumns {
         companion object {
-            val TABLE_NAME = "phone_log"
-            val COLUMN_LOG_PHONE_NUMBER = "number"
-            val COLUMN_LOG_PHONE_TIME= "time"
-            val COLUMN_LOG_PHONE_DATE = "date"
+            const val TABLE_NAME = "phone_log"
+            const val COLUMN_LOG_PHONE_NUMBER = "number"
+            const val COLUMN_LOG_PHONE_TIME= "time"
+            const val COLUMN_LOG_PHONE_DATE = "date"
         }
     }
 
     class PhoneLogTagsEntry : BaseColumns {
         companion object {
-            val TABLE_NAME = "tags"
-            val COLUMN_PHONE_LOG_TAGS_NUMBER = "number"
-            val COLUMN_PHONE_LOG_TAGS_TAG = "tag"
+            const val TABLE_NAME = "tags"
+            const val COLUMN_PHONE_LOG_TAGS_NUMBER = "number"
+            const val COLUMN_PHONE_LOG_TAGS_TAG = "tag"
         }
     }
 
     class TokenEntry : BaseColumns {
         companion object {
-            val TABLE_NAME = "tokens"
-            val COLUMN_AES_KEY = "aes_key"
-            val COLUMN_ANDROID_OS = "android_os"
-            val COLUMN_DEVICE_ID = "device_id"
-            val COLUMN_IS_ACTIVE = "is_active"
-            val COLUMN_PRIVATE_KEY = "private_key"
-            val COLUMN_REMAIN_COUNT = "remain_count"
-            val COLUMN_TOKEN = "token"
-            val COLUMN_IS_PRIMARY_USE = "is_primary_use"
+            const val TABLE_NAME = "tokens"
+            const val COLUMN_AES_KEY = "aes_key"
+            const val COLUMN_ANDROID_OS = "android_os"
+            const val COLUMN_DEVICE_ID = "device_id"
+            const val COLUMN_IS_ACTIVE = "is_active"
+            const val COLUMN_PRIVATE_KEY = "private_key"
+            const val COLUMN_REMAIN_COUNT = "remain_count"
+            const val COLUMN_TOKEN = "token"
+            const val COLUMN_IS_PRIMARY_USE = "is_primary_use"
         }
     }
 }
