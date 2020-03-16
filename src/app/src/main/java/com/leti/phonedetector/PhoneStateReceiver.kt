fb111c23b2125005e073707156c322c4fe61ef50
kovinevmv <kovinevmv@gmail.com>
2020-03-16 14:27:31 +0300

    fix: change default switch

--- src/app/src/main/java/com/leti/phonedetector/PhoneStateReceiver.kt
+++ src/app/src/main/java/com/leti/phonedetector/PhoneStateReceiver.kt
@@ -110,7 +110,7 @@ class PhoneStateReceiver : BroadcastReceiver() {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
         val timeout = sharedPreferences.getInt("detection_delay_seekbar", 5)
         val isNetworkOnly = sharedPreferences.getBoolean("use_only_network_info",false)
-        val noCacheEmpty = sharedPreferences.getBoolean("no_cache_empty_phones", false)
+        val noCacheEmpty = sharedPreferences.getBoolean("no_cache_empty_phones", true)
 
         val db = PhoneLogDBHelper(context)
 
