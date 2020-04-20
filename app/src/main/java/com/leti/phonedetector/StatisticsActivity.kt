be5df4cd6606440203a7b00071fa697f946fc5a8
kovinevmv <kovinevmv@gmail.com>
2020-04-20 16:42:22 +0300

    feat: add some diagrams

--- app/src/main/java/com/leti/phonedetector/StatisticsActivity.kt
+++ app/src/main/java/com/leti/phonedetector/StatisticsActivity.kt
@@ -1,41 +1,59 @@
 package com.leti.phonedetector
 
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.anychart.APIlib
 import com.anychart.AnyChart
 import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.CategoryValueDataEntry
 import com.anychart.chart.common.dataentry.DataEntry
 import com.anychart.chart.common.dataentry.ValueDataEntry
-import com.anychart.enums.Anchor
-import com.anychart.enums.HoverMode
-import com.anychart.enums.Position
-import com.anychart.enums.TooltipPositionMode
+import com.anychart.chart.common.listener.Event
+import com.anychart.chart.common.listener.ListenersInterface
+import com.anychart.enums.*
+import com.anychart.scales.OrdinalColor
 import com.leti.phonedetector.database.PhoneLogDBHelper
+import com.leti.phonedetector.model.PhoneLogInfo
 import kotlinx.android.synthetic.main.activity_statistics.*
 import java.util.*
 
 
 class StatisticsActivity : AppCompatActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.N)
+    private lateinit var phones: ArrayList<PhoneLogInfo>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
+
         title = "Statistics"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val spamView: AnyChartView = findViewById(R.id.any_chart_spam)
-        APIlib.getInstance().setActiveAnyChartView(any_chart_spam)
-        // anyChartView.setProgressBar(findViewById(R.id.progress_bar))
+        readDataBase()
+
+        createSpamChart()
+        createPieChart()
+        createWordCloud()
+    }
+
+    private fun readDataBase() {
+        val db = PhoneLogDBHelper(this)
+        phones = db.readPhoneLog()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun createSpamChart() {
+        val spamView: AnyChartView = findViewById(R.id.chart_spam)
+        APIlib.getInstance().setActiveAnyChartView(chart_spam)
+        spamView.setProgressBar(findViewById(R.id.progress_bar_chart_spam))
 
         val spam = AnyChart.column()
 
-        var db = PhoneLogDBHelper(this)
-        var phones = db.getStatisticsPhoneLog()
+
         val calendar: Calendar = Calendar.getInstance()
         val currentYear: Int = calendar.get(Calendar.YEAR)
         val currentMonth: Int = calendar.get(Calendar.MONTH)
@@ -54,19 +72,17 @@ class StatisticsActivity : AppCompatActivity() {
                 phonesMap[currentYear.toString() + '.' + i.toString().padStart(2, '0')] = 0
             }
         }
-        phones.forEach{
-            Log.d("PHONES NUM", it.date.substring(0, 7))
-            Log.d("PHONES NUM EQ", phonesMap.containsKey(it.date.substring(0, 7)).toString())
-            if (phonesMap.containsKey(it.date.substring(0, 7))) {
-                phonesMap.merge(it.date.substring(0, 7),1, Int::plus)
-            }
+
+        val innerPhones = phones.filter { p -> p.isSpam }
+        for (phone in innerPhones) {
+            val date = phone.date.substring(0, 7)
+            phonesMap[date] = phonesMap.getOrPut(date) { 1 } + 1
         }
 
         val spamData: MutableList<DataEntry> = ArrayList()
 
         for ((k, v) in phonesMap) {
-            Log.d("PHONES MAP", "$k = $v")
-            spamData.add(ValueDataEntry(k.substring(5,7) + '.' + k.substring(2, 4), v))
+            spamData.add(ValueDataEntry(k.substring(5, 7) + '.' + k.substring(2, 4), v))
         }
 
         var column = spam.column(spamData)
@@ -80,7 +96,7 @@ class StatisticsActivity : AppCompatActivity() {
             .format("{%Value} calls")
 
         spam.animation(true)
-        spam.title("Spam calls for 12 latest months")
+        spam.title("Spam calls for 12 latest months").enabled(true)
 
         spam.yScale().minimum(0.0)
         spam.yScale().ticks().allowFractional(false)
@@ -88,7 +104,7 @@ class StatisticsActivity : AppCompatActivity() {
         spam.yAxis(0).labels().format("{%Value}{groupsSeparator: }")
         spam.xAxis(0).labels().format("{%Value}{groupsSeparator: }")
 
-        var xAxisLabels = spam.xAxis(0).labels()
+        val xAxisLabels = spam.xAxis(0).labels()
         xAxisLabels.rotation(270)
 
         spam.tooltip().positionMode(TooltipPositionMode.POINT)
@@ -100,80 +116,93 @@ class StatisticsActivity : AppCompatActivity() {
         spam.yGrid(0).enabled(true)
 
         spamView.setChart(spam)
-
-        val numberView: AnyChartView = findViewById(R.id.any_chart_number)
-        APIlib.getInstance().setActiveAnyChartView(any_chart_number)
-        // anyChartView.setProgressBar(findViewById(R.id.progress_bar))
-
-        val number = AnyChart.column()
-
-        db = PhoneLogDBHelper(this)
-        phones = db.getStatisticsPhoneLog()
-
-        phonesMap = mutableMapOf<String, Int>()
-
-        if (currentMonth == 11) {
-            for (i in 1..12) {
-                phonesMap[currentYear.toString() + '.' + i.toString().padStart(2, '0')] = 0
-            }
-        } else {
-            for (i in (currentMonth + 2)..12) {
-                phonesMap[(currentYear - 1).toString() + '.' + i.toString().padStart(2, '0')] = 0
-            }
-            for (i in 1..(currentMonth + 1)) {
-                phonesMap[currentYear.toString() + '.' + i.toString().padStart(2, '0')] = 0
-            }
-        }
-        phones.forEach{
-            Log.d("PHONES NUM", it.date.substring(0, 7))
-            Log.d("PHONES NUM EQ", phonesMap.containsKey(it.date.substring(0, 7)).toString())
-            if (phonesMap.containsKey(it.date.substring(0, 7))) {
-                phonesMap.merge(it.date.substring(0, 7),1, Int::plus)
-            }
-        }
-
-        val numberData: MutableList<DataEntry> = ArrayList()
-
-        for ((k, v) in phonesMap) {
-            Log.d("PHONES MAP", "$k = $v")
-            numberData.add(ValueDataEntry(k.substring(5,7) + '.' + k.substring(2, 4), v))
-        }
-
-        column = number.column(numberData)
-
-        column.tooltip()
-            .titleFormat("{%X}")
-            .position(Position.CENTER_BOTTOM)
-            .anchor(Anchor.CENTER_BOTTOM)
-            .offsetX(0.0)
-            .offsetY(10.0)
-            .format("{%Value} calls")
-
-        number.animation(true)
-        number.title("Spam calls for 12 latest months")
-
-        number.yScale().minimum(0.0)
-        number.yScale().ticks().allowFractional(false)
-
-        number.yAxis(0).labels().format("{%Value}{groupsSeparator: }")
-        number.xAxis(0).labels().format("{%Value}{groupsSeparator: }")
-
-        xAxisLabels = number.xAxis(0).labels()
-        xAxisLabels.rotation(270)
-
-        number.tooltip().positionMode(TooltipPositionMode.POINT)
-        number.interactivity().hoverMode(HoverMode.BY_X)
-
-        number.xAxis(0).title("Month")
-        number.yAxis(0).title("Number of calls")
-
-        number.yGrid(0).enabled(true)
-
-        numberView.setChart(number)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
+    private fun createPieChart() {
+        val chart: AnyChartView = findViewById(R.id.chart_pie_top)
+        APIlib.getInstance().setActiveAnyChartView(chart_pie_top)
+
+        chart.setProgressBar(findViewById(R.id.progress_bar_chart_pie_top));
+
+        val pie = AnyChart.pie()
+
+        pie.setOnClickListener(object :
+            ListenersInterface.OnClickListener(arrayOf("x", "value")) {
+            override fun onClick(event: Event) {}
+        })
+
+        val data: MutableList<DataEntry> = ArrayList()
+        data.add(
+            ValueDataEntry(
+                "Spam incoming calls",
+                phones.filter { phone -> phone.isSpam }.size
+            )
+        )
+        data.add(
+            ValueDataEntry(
+                "Not spam incoming calls",
+                phones.filter { phone -> !phone.isSpam }.size
+            )
+        )
+
+
+        pie.data(data)
+        pie.title("Spam Call Pie Chart")
+        pie.labels().position("outside")
+
+
+        pie.legend()
+            .position("center-bottom")
+            .itemsLayout(LegendLayout.HORIZONTAL)
+            .align(Align.CENTER)
+
+
+        chart.setChart(pie)
+    }
+
+    private fun createWordCloud() {
+        val anyChartView: AnyChartView = findViewById(R.id.chart_word_cloud)
+        APIlib.getInstance().setActiveAnyChartView(chart_word_cloud)
+
+        anyChartView.setProgressBar(findViewById(R.id.progress_bar_chart_word_cloud))
+
+        val tagCloud = AnyChart.tagCloud()
+
+        tagCloud.title("World Population")
+
+        val ordinalColor = OrdinalColor.instantiate()
+        ordinalColor.colors(
+            arrayOf(
+                "#f14526", "#26959f", "#3b8ad8", "#60727b", "#e24b26"
+            )
+        )
+        tagCloud.colorScale(ordinalColor)
+        tagCloud.angles(arrayOf(-90.0, 0.0, 90.0))
+
+        tagCloud.colorRange().enabled(true)
+        tagCloud.colorRange().colorLineSize(15.0)
+
+
+        val data: MutableList<DataEntry> = ArrayList()
+
+        val phonesMap = mutableMapOf<Boolean, MutableMap<String, Int>>()
+        phonesMap[true] = mutableMapOf()
+        phonesMap[false] = mutableMapOf()
+
+        for (phone in phones) {
+            phonesMap[phone.isSpam]?.set(phone.name,
+                (phonesMap[phone.isSpam]?.getOrPut(phone.name) { 0 } ?: 0) + 1)
+        }
+
+        for ((isSpam, v) in phonesMap) {
+            for ((name, count) in v) {
+                data.add(CategoryValueDataEntry(name, if (isSpam) "Spam" else "Not spam", count))
+            }
+        }
+
+
+        tagCloud.data(data)
+
+        anyChartView.setChart(tagCloud)
     }
 }
